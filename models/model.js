cc86c4ccdd9b10ab0bcdf12b05e6ad531ace2b7f
developereswar <developereswar94@gmail.com>
2025-07-24 10:40:36 +0530

    add db configs

--- models/model.js
+++ models/model.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const todoSchema = new mongoose.Schema({
+  id: { type: String, default: uuidv4, unique: true },
   details: { type: String, required: true },
-  completed: { type: Boolean, default: false },
-}, { timestamps: true });
+  completed: { type: Boolean, default: false }
+});
 
 module.exports = mongoose.model('Todo', todoSchema);
